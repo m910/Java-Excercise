4a60fc4abc8dd6f7733f05db23a1c46ec324b442
Megha <megha.kothale@cgi.com>
2019-07-11 14:56:03 +0530

    ReverseStringDone

--- src/Main/Java/com/stackroute/ReverseString.java
+++ src/Main/Java/com/stackroute/ReverseString.java
@@ -6,7 +6,7 @@ public class ReverseString {
     public static void main(String[] args)
     {
         String x="";
-        System.out.println("Enter the string");
+        System.out.println("Enter the string :");
         Scanner scan=new Scanner(System.in);
         String s=scan.nextLine();
         for(int i=s.length()-1;i>=0;i--)
